f08f586d314df03dd02181a915c36701a68f7b87
Jake Hoopes <hoopesjake@gmail.com>
2025-04-02 22:41:59 -0600

    fixed cart

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,11 @@ import App from './App.tsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { CartProvider } from './CartContext';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </BrowserRouter>
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>
 );
